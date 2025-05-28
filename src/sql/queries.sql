0568f5557399a02bd735f77a4c5c9da1681c07a7
infokaren20 <114136329+infokaren20@users.noreply.github.com>
2025-05-28 14:15:08 +0000

    trabajo_finalizado

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -7,33 +7,66 @@ SELECT * FROM species;
 SELECT * FROM climate;
 SELECT * FROM observations;
 
-
--- MISSION 1
--- Your query here
-
--- MISSION 2
--- Your query here:
-
-
--- MISSION 3
--- Your query here:
+-- Misión 1: 
+-- MISSION 1;
+select country , species_id from regions
+INNER JOIN observations ON regions.id = observations.id
+GROUP BY country
+ORDER BY species_id desc
+LIMIT 5
 
 
--- MISSION 4
--- Your query here:
+-- MISSION 2:
+-- Your query here;
+
+select observation_date  from observations
+GROUP BY observation_date
+ORDER BY observation_date;
 
 
--- MISSION 5
--- Your query here:
 
 
--- MISSION 6
--- Your query here:
+
+-- MISSION 3: 
+-- Your query here;
+select common_name, count from species
+INNER JOIN observations ON species.id = observations.id
+ORDER BY count asc;
 
 
--- MISSION 7
--- Your query here:
+-- MISSION 4:
+-- Your query here;
+select name, common_name from species
+INNER JOIN regions ON species.id = regions.id
+ORDER BY name asc
+limit 5;
 
 
--- MISSION 8
--- Your query here:
+-- MISSION 5: 
+-- Your query here;  
+ select scientific_name, observer from species
+ INNER JOIN observations ON species.id = observations.species_id
+ ORDER BY observer asc;
+
+
+-- MISSION 6:  
+-- Your query here;
+
+
+-- MISSION 7: 
+-- Your query here;
+
+select scientific_name, species_id, observer from species
+INNER JOIN observations ON species_id = observations.species_id
+where  observer IS NULL;
+
+
+
+
+-- MISSION 8: 
+-- Your query here;
+
+select observation_date, observer, scientific_name from species
+INNER JOIN observations ON species_id = observations.id
+ORDER BY observation_date asc;
+
